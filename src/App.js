ae113442e92c21be5a3da5cf1abae7452b098b30
tusker4 <tusker4@naver.com>
2023-07-12 19:49:47 +0900

    Add : 연습적용

--- src/App.js
+++ src/App.js
@@ -4,36 +4,47 @@
 // // css 적용한다
 import React from 'react';
 
-function Header() {
+function Header(props) {
+  // console.log('props', props, props.title);
   return <header className="App-header">
-    <h1><a href="/">React2</a></h1>
+    <h1><a href="/">{props.titla}</a></h1>
   </header>
 }
-function Nav() {
+function Nav(props) {
+  const lis = [
+    <li><a href="/read/1">html2</a></li>,
+    <li><a href="/read/2">css</a></li>,
+    <li><a href="/read/3">js</a></li>
+  ]
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)
+  }
   return <nav>
     <ol>
-      <li><a href="/read/1">html</a></li>
-      <li><a href="/read/2">css</a></li>
-      <li><a href="/read/3">js</a></li>
+      {lis}
     </ol>
   </nav>
 }
-function Article() {
+function Article(props) {
   return <article>
-    <h2>Welcome</h2>
-    Hello, Web
+    <h2>{props.title}</h2>
+    Hello, Website
   </article>
 }
 
 function App() {
+  const topics = [
+    { id: 1, title: 'html', body: 'html is ....' },
+    { id: 2, title: 'css', body: 'css is ....' },
+    { id: 3, title: 'js', body: 'js is ....' }
+  ]
   return (
     <div className="App">
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header titla="React"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Hellcome"></Article>
+      <Article title="Welcomes"></Article>
     </div>
   );
 }
